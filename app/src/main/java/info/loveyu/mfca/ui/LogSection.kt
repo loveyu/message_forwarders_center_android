2aa691b9d4e18cb29a212f3d84a2fb83aa309603
loveyu <i@loveyu.info>
2026-04-16 14:01:43 +0800

    feat(ui): 为日志条目添加长按复制功能
    - 在 LogSection.kt 中为日志条目添加 detectTapGestures 支持
    - 通过 pointerInput 修饰符实现长按手势检测
    - 长按时将日志内容复制到剪贴板并显示 Toast 提示
    - 优化导入语句顺序以符合代码规范

--- app/src/main/java/info/loveyu/mfca/ui/LogSection.kt
+++ app/src/main/java/info/loveyu/mfca/ui/LogSection.kt
@@ -7,6 +7,7 @@ import android.content.ClipboardManager
 import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -32,11 +33,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.gestures.detectTapGestures
 import info.loveyu.mfca.R
 import info.loveyu.mfca.util.LogLevel
 import info.loveyu.mfca.util.LogManager
